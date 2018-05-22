f23f8c8ff8c172053421252c082a4f6f53ace1f4
matteodevigili <39398896+matteodevigili@users.noreply.github.com>
2018-05-22 08:53:19 +0200

    Update DatabaseManagement.php

--- Code/PHP/Database/DatabaseManagement.php
+++ Code/PHP/Database/DatabaseManagement.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of DatabaseManagement
  *
  * @author LORENZO.ORSINGHER
  */
+
 class DatabaseManagement {
     
     private $connection;
