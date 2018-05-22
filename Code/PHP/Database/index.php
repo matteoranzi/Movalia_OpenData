b0e093ad71b8958b3057c7f22b888d5263102c29
giannibenigni <39398690+giannibenigni@users.noreply.github.com>
2018-05-22 08:32:41 +0200

    add functions

--- Code/PHP/Database/index.php
+++ Code/PHP/Database/index.php
@@ -11,24 +11,36 @@
             include 'ParserJSON.php';
             include 'CarParking.php';
             include 'DatabaseManagement.php';
+            include 'ParserXML.php';
             
-            $biketn = "bikesharingtrento";
-            $parkingtorino = "carparkingtorino";
+            $bikeTN = "bikesharingtrento";
+            $parkingTO = "carparkingtorino";
+            
             define("TRENTO_BIKE_SHARING_URL", "https://os.smartcommunitylab.it/core.mobility/bikesharing/trento");
             define("TORINO_PARKING_DATA_URL", "http://opendata.5t.torino.it/get_pk");
             
             $connection = mysqli_connect("localhost", "root", "", "opendata", 3306);         
             
             
-            
             $databaseManager = new DatabaseManagement(($connection));
-            $parser = new ParserJSON(TRENTO_BIKE_SHARING_URL);
             
-            $array = $parser->parse();
+            $parserJSON = new ParserJSON(TRENTO_BIKE_SHARING_URL);
+            $parserXML = new ParserXML(TORINO_PARKING_DATA_URL);
+            
+            $arrayTN = $parserJSON->parse();
+            $arrayTO = $parserXML->parse();
                         
-            $databaseManager->clearTable($biketn);
-            $databaseManager->addBikeSharing($array);
-            $databaseManager->printAllTN($biketn);
+            $databaseManager->clearTable($bikeTN);
+            $databaseManager->clearTable($parkingTO);
+            
+            $databaseManager->addBikeSharing($arrayTN);
+            $databaseManager->addParkingTO($arrayTO);
+            
+            $databaseManager->printAllTN($bikeTN);
+            echo "<br><br><br>";
+            $databaseManager->printAllTO($parkingTO);
+            
+            
             
 ?>
             
