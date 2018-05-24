6ba8b84fc93387119ea27361f30d7e2cea067916
Matteo Ranzi <matteoranzi2000@gmail.com>
2018-05-24 11:24:17 +0200

    Add bash script to add index.php into crontab
    The index.php script is executed evey 5 minutes

---
+++ Code/start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#5 times every minute
+FREQUENCY=5;
+
+interpreter="/usr/bin/php"
+
+if [ -e $interpreter ]; then
+
+  tmpDir=$(mktemp --directory)
+  #get the full path of the dir
+  scriptPath="$( cd "$(dirname "$0")" ; pwd -P )"
+  phpScript="$scriptPath/index.php"
+  cronjob="*/$FREQUENCY * * * * $interpreter $phpScript"
+  
+  crontab -l > $tmpDir/crontab.backup
+  echo "$cronjob" >> $tmpDir/crontab.backup
+
+  crontab $tmpDir/crontab.backup
+
+  echo "New crontab installed at $phpScript"
+else
+  echo "Error: PHP interpreter not found at $interpreter"
+fi
